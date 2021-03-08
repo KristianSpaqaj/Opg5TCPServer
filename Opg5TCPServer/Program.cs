4fae6445a679a528222d950bea09453d63fafb5f
KristianSpaqaj <kris49j7@edu.easj.dk>
2021-03-08 10:02:56 +0100

    Add project files.

---
+++ Opg5TCPServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using Obl5TCPServer;
+using System;
+
+namespace Opg5TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Server.Start();
+        }
+    }
+}
